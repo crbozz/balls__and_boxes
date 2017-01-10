64e6978f75bc5ef6dfe7f57ff41a4e461870e327
crbozz <crbozz@gmail.com>
2017-01-09 23:20:27 -0400

    Camera movement changed to rotate and zoom

--- BallsAndBoxes/Assets/scripts/CameraController.cs
+++ BallsAndBoxes/Assets/scripts/CameraController.cs
@@ -9,8 +9,15 @@ public class CameraController : MonoBehaviour {
 		float moveH = Input.GetAxis ("Horizontal");
 		float moveV = Input.GetAxis ("Vertical");
 
-		Vector3 move = new Vector3 (-moveV, 0, moveH);
+		//Vector3 move = new Vector3 (-moveV, 0, moveH);
 
-		transform.position = transform.position + move;
+		//transform.position = transform.position + move;
+
+		transform.RotateAround (Vector3.zero, Vector3.up, moveH);
+
+		var fw = transform.position + (transform.forward * moveV);
+		if (fw.y > 12 && fw.y < 25) {
+			transform.position = fw;
+		}
 	}
 }
