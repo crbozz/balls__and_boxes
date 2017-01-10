17474f8934ec8b1e9ba0001031f65468a87ea8bb
crbozz <crbozz@gmail.com>
2017-01-10 00:11:36 -0400

    Adding a new ball after left-click
    Now the game starts with no ball waiting for a mouse left-button click. A
    new ball will be created after each click. The number of balls was limited
    to five.

---
+++ BallsAndBoxes/Assets/scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject ball;
+	public int maxBalls;
+
+	private int numBalls;
+
+	void Start () {
+		numBalls = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (numBalls < maxBalls) {
+				Instantiate (ball);
+				numBalls++;
+			}
+		}
+	}
+}
