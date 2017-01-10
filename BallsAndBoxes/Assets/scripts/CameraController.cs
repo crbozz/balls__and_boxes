9baee47dad288d237bd0da5ec5cbb0264d6a8023
crbozz <crbozz@gmail.com>
2017-01-09 22:45:46 -0400

    Initial version

---
+++ BallsAndBoxes/Assets/scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		Vector3 move = new Vector3 (-moveV, 0, moveH);
+
+		transform.position = transform.position + move;
+	}
+}
